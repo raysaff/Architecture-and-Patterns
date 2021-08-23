a76ab9e897f5721abbb053f582968c28d57daab8
Ray_Saff <sav777@yandex.ru>
2021-08-23 16:58:19 +0700

    HW3 done

---
+++ 2D Asteroids/Assets/Scripts/Pools/PoolGO.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolGO<T> where T : Transform
+{
+    public T _prefab { get; }
+    public bool autoExpand { get; set; }
+    public Transform _container { get; }
+
+    private List<T> _pool;
+
+    public PoolGO(T prefab, int capacity)
+    {
+        _prefab = prefab;
+        _container = null;
+
+        CreatePool(capacity);
+    }
+
+    public PoolGO(T prefab, int capacity, Transform container)
+    {
+        _prefab = prefab;
+        _container = container;
+
+        CreatePool(capacity);
+    }
+
+    private void CreatePool(int capacity)
+    {
+        _pool = new List<T>();
+
+        for (int i = 0; i < capacity; i++)
+        {
+            CreateObject();
+        }
+    }
+
+    private T CreateObject(bool isActiveByDefault = false)
+    {
+        var createdObject = UnityEngine.Object.Instantiate(_prefab, _container);
+        createdObject.gameObject.SetActive(isActiveByDefault);
+        _pool.Add(createdObject);
+        return createdObject;
+    }
+
+    public bool HasFreeElenet(out T element)
+    {
+        foreach (var mono in _pool)
+        {
+            if (!mono.gameObject.activeInHierarchy)
+            {
+                mono.gameObject.SetActive(true);
+                element = mono;
+                return true;
+            }
+        }
+        element = null;
+        return false;
+    }
+
+    public T GetFreeElement()
+    {
+        if (HasFreeElenet(out var element))
+            return element;
+
+        if (autoExpand)
+            return CreateObject(true);
+
+        throw new Exception($"Нет свободных элементов {typeof(T)}");
+    }
+}
