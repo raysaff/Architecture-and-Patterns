96808cc02f1625ef063f7ca3f484020ef9785ead
Ray_Saff <sav777@yandex.ru>
2021-08-25 13:02:09 +0700

    HW4 1.2

---
+++ 2D Asteroids/Assets/Scripts/BuilderExtensions/GameObjectBuilder.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+internal class GameObjectBuilder
+{
+    protected GameObject _gameObject;
+
+    public GameObjectBuilder() => _gameObject = new GameObject();
+
+    protected GameObjectBuilder(GameObject gameObject) => _gameObject = gameObject;
+
+    public GameObjectVisualBuilder Visual => new GameObjectVisualBuilder(_gameObject);
+    public GameObjectPhysicsBuilder Physics => new GameObjectPhysicsBuilder(_gameObject);
+
+    public static implicit operator GameObject(GameObjectBuilder builder) => builder._gameObject;
+
+}
